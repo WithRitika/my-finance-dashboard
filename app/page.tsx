32528c82a4278db47529294d988029f72e04f252
Ritika Soni <withritikasoni@gmail.com>
2026-06-11 00:19:48 +0530

    feat: add Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 // import Image from "next/image";
-
+import Navbar from "@/components/layout/Navbar";
 export default function Home() {
   return (
-   <main className="flex items-center min-h-screen justify-center bg-gray-950">
-    <h1 className="text-4xl font-bold text-white">
-      Finance Dashboard 🚀
-    </h1>
+   <main className="min-h-screen bg-gray-950">
+    <Navbar/>
+     <div className="flex items-center justify-center h-[90vh]">
+        <h1 className="text-4xl font-bold text-white">
+          Finance Dashboard 🚀
+        </h1>
+      </div>
    </main>
   );
 }
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar =()=>{
+    return (<nav className="w-full bg-gray-900 border-b border-gray-800 px-6 py-4 flex items-center justify-between">
+        <h1 className="text-white font-bold text-xl">
+            💰 Finance Dashboard
+        </h1>
+        <div className="flex items-center gap-4">
+            <span className="text-gray-400 text-sm">Welcome, Ritika</span>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white text-sm px-4 py-2 rounded-lg">Logout</button>
+        </div>
+    </nav>);
+};
+
+export default Navbar;
